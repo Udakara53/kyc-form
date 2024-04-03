e1151f3a0fbde78d8798f5ffee12a12027b16d43
Udakara53 <udakaratharindu@gmail.com>
2024-04-03 15:09:48 +0530

    clear the component header

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="header">
-    <v-img width="100vw" height="312px" cover src="../assets/cover.png ">
+    <v-img
+      width="100vw"
+      height="312px"
+      aspect-ratio="1728/544"
+      cover
+      src="../assets/cover.png"
+    >
       <div class="d-flex flex-row justify-space-between pa-5 px-9">
-        <v-sheet class="ma-2 pa-2" style="background-color: transparent">
+        <v-sheet class="" style="background-color: transparent">
           <v-img
-            class="alignment"
+            class="align-start"
             width="60px"
             height="30.77px"
             src="../assets/logoKYC.png"
@@ -14,42 +20,6 @@
         <v-sheet class="ma-2 px-7 py-2 contactBtn"><ContactBtn /></v-sheet>
       </div>
     </v-img>
-    <!-- <v-sheet fluid class="u-top">
-      <v-img
-        width="100vw"
-        height="312px"
-        cover
-        src="../assets/cover.png"
-      ></v-img>
-
-      <v-col
-        cols="12"
-        style="background-color: blue; display: flex; flex-direction: row"
-        class="upper"
-      >
-        <v-col
-          cols="3"
-          style="background-color: red; height: 20px; width: 20px"
-        >
-        </v-col>
-        <v-col
-          cols="3"
-          style="background-color: red; height: 20px; width: 20px"
-        ></v-col>
-        <div>
-        <v-img
-            class="alignment"
-            width="60px"
-            height="18.77px"
-            src="../assets/logoKYC.png"
-          >
-          </v-img>
-        </div>
-        <div class="contact">
-          <ContactBtn />
-        </div>
-      </v-col>
-    </v-sheet> -->
   </div>
 </template>
 
@@ -64,22 +34,6 @@ export default {
 </script>
 
 <style socped>
-.u-top {
-  position: relative;
-}
-.upper {
-  justify-content: space-between;
-  position: absolute;
-  outline: 1px solid red;
-  top: 16px;
-  height: 49px;
-}
-.upper .alignment {
-  margin: 10px;
-}
-.contact {
-  width: 100%;
-}
 .contactBtn {
   background: #f54d4d;
   border-radius: 20px;
