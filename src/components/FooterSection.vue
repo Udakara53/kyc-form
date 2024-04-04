78a9051de3dd7607686b74eaec152dec8363e10d
Udakara53 <udakaratharindu@gmail.com>
2024-04-04 16:09:13 +0530

    footer section is done. has to adjust adress line

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -3,19 +3,27 @@
     class="pt-7"
     style="aspect-ratio: 1728 / 408; max-width: 100vw; background: #f54d4d"
   >
-    <div class="d-flex justify-space-around mb-9" style="background: green">
+    <div class="d-flex justify-space-around mb-6">
       <v-sheet
-        class="d-flex ma-2 pa-5 flex-grow-1"
-        style="align-items: center; justify-content: center"
+        class="d-flex ma-2 pa-5"
+        style="
+          align-items: center;
+          justify-content: center;
+          width: 40%;
+          background: transparent;
+        "
       >
         <v-img
           class="v-center"
-          width="60px"
-          height="19%"
-          src="../assets/logoKYC.png"
+          width="35%"
+          height="36.5%"
+          src="../assets/LogoKYC-white.png"
         />
       </v-sheet>
-      <v-sheet class="d-flex flex-column ma-2 pa-2 flex-grow-1">
+      <v-sheet
+        class="d-flex flex-column ma-2 pa-2"
+        style="width: 20%; background: transparent"
+      >
         <div class="">
           <p
             style="
@@ -24,15 +32,13 @@
               font-family: 'Poppins', sans-serif;
               font-size: 24px;
               font-weight: 600;
+              color: white;
             "
           >
             Contact US
           </p>
         </div>
-        <div
-          style="background: green; align-self: flex-start"
-          class="d-flex mt-2 mb-2"
-        >
+        <div style="align-self: flex-start" class="d-flex mt-2 mb-2">
           <v-img width="20.41px" height="20px" src="../assets/Frame.png">
           </v-img>
           <p>
@@ -41,10 +47,7 @@
             >
           </p>
         </div>
-        <div
-          style="background: blue; align-self: flex-start"
-          class="d-flex mt-1"
-        >
+        <div style="align-self: flex-start" class="d-flex mt-1">
           <v-img width="20.41px" height="20px" src="../assets/Message.png">
           </v-img>
           <p>
@@ -53,10 +56,7 @@
             >
           </p>
         </div>
-        <div
-          style="background: brown; align-self: flex-start; display: flex"
-          class="my-2"
-        >
+        <div style="align-self: flex-start; display: flex" class="my-2">
           <v-img width="20.41px" height="20px" src="../assets/Location.png">
           </v-img>
 
@@ -69,11 +69,95 @@
           </p>
         </div>
       </v-sheet>
-      <v-sheet class="ma-2 pa-2 flex-grow-1"> logo </v-sheet>
+      <!-- add social-media icon -->
+      <v-sheet class="ma-2 pa-2" style="width: 20%; background: transparent">
+        <div class="mb-2">
+          <p
+            style="
+              text-align: left;
+              font-family: 'Poppins';
+              font-family: 'Poppins', sans-serif;
+              font-size: 24px;
+              font-weight: 600;
+              color: white;
+            "
+          >
+            Social Media
+          </p>
+        </div>
+        <div class="d-flex">
+          <div class="icon-outer">
+            <div class="icon-inner">
+              <v-img
+                width="20px"
+                height="14px"
+                src="../assets/Youtube.png"
+              ></v-img>
+            </div>
+          </div>
+          <div class="icon-outer mx-2">
+            <div class="icon-inner">
+              <v-img
+                width="20px"
+                height="14px"
+                src="../assets/instagram.png"
+              ></v-img>
+            </div>
+          </div>
+          <div class="mx-2 icon-outer">
+            <div class="icon-inner">
+              <v-img
+                width="20px"
+                height="14px"
+                src="../assets/Tweeter.png"
+              ></v-img>
+            </div>
+          </div>
+          <div class="mx-2 icon-outer">
+            <div class="icon-inner">
+              <v-img
+                width="20px"
+                height="14px"
+                src="../assets/Facebook.png"
+              ></v-img>
+            </div>
+          </div>
+          <div class="mx-2 icon-outer">
+            <div class="icon-inner">
+              <v-img
+                width="20px"
+                height="14px"
+                src="../assets/Linkedin.png"
+              ></v-img>
+            </div>
+          </div>
+        </div>
+      </v-sheet>
+      <!-- end social-media icon -->
+      <!-- start footer -->
+      <v-sheet
+        class="d-flex ma-2 pa-5"
+        style="
+          align-items: center;
+          justify-content: center;
+          width: 20%;
+          background: transparent;
+        "
+      >
+      </v-sheet>
+      <!-- end footer -->
     </div>
-    <div class="d-flex justify- mb-1">
-      <v-sheet class="ma-2 pa-2 flex-grow-1" style="height: 125">
-        justify-space-around
+    <div class="d-flex mb-1">
+      <v-sheet
+        class="ma-2 pa-2 flex-grow-1"
+        style="height: 125; background: transparent"
+      >
+        <div class="horizontal-line"></div>
+        <div class="mt-3">
+          <p class="poppins-paragraph">
+            Copyright ©2023 PayMedia. All Rights Reserved
+          </p>
+        </div>
       </v-sheet>
     </div>
   </div>
@@ -88,5 +172,31 @@ export default {
 .logo-align {
   align-items: center;
   justify-content: center;
+}
+.icon-outer {
+  position: relative;
+  width: 40px;
+  height: 40px;
+  border: 1.5px solid #ccc;
+  border-radius: 50%;
+}
+.icon-inner {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.horizontal-line {
+  width: 82.6%;
+  margin: 0 auto;
+  border-top: 2px solid #fff;
+}
+.poppins-paragraph {
+  font-family: 'Poppins', sans-serif;
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 27px;
+  text-align: center;
+  color: white;
 }
 </style>
